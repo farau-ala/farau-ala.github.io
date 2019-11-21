28d3b861db61fd4b888429367e28b9aaa7616667
= <=>
2019-11-21 09:30:07 +0700

    9435

---
+++ precache-manifest.4011fae3f97bb27f0083fa20b6c87514.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e2a0c31f439f2906d8",
+    "url": "/css/chunk-vendors.f7fda0c6.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "6dd16196cc5b86d1e4193fdafba18bab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c21103591abd6e228ca",
+    "url": "/js/app.16598feb.js"
+  },
+  {
+    "revision": "e8e2a0c31f439f2906d8",
+    "url": "/js/chunk-vendors.f68b23d0.js"
+  },
+  {
+    "revision": "f5063b9478cf532d7196f0c99e04827b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
